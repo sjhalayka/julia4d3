8729081bc344a317ac331de901a83ae95236aaf8
Shawn Halayka <sjhalayka@gmail.com>
2020-04-19 16:10:24 -0600

    Add files via upload

--- Release/flat.vs.glsl
+++ Release/flat.vs.glsl
@@ -1,11 +1,17 @@
 #version 410 core
 
-
 // Per-vertex inputs
 layout (location = 0) in vec4 position;
 
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
+uniform vec3 flat_colour;
+
+out VS_OUT
+{
+    vec3 vertex_colour;
+} vs_out;
+
 
 void main(void)
 {
@@ -14,4 +20,6 @@ void main(void)
 
     // Calculate the clip-space position of each vertex
     gl_Position = proj_matrix * P;
+
+    vs_out.vertex_colour = flat_colour;
 }
