703d32f4cd5b4610927780b819fddead00020029
Shawn Halayka <sjhalayka@gmail.com>
2020-11-15 00:30:50 -0600

    Add files via upload

---
+++ points.vs.glsl
@@ -0,0 +1,40 @@
+#version 410 core
+
+// Per-vertex inputs
+layout (location = 0) in vec4 position0;
+layout (location = 1) in vec4 position1;
+layout (location = 2) in vec4 position2;
+layout (location = 3) in vec4 position3;
+layout (location = 4) in vec4 position4;
+layout (location = 5) in vec4 position5;
+layout (location = 6) in vec4 position6;
+layout (location = 7) in vec4 position7;
+
+uniform float threshold;
+
+out VS_OUT
+{
+    vec4 position0;
+    vec4 position1;
+    vec4 position2;
+    vec4 position3;
+    vec4 position4;
+    vec4 position5;
+    vec4 position6;
+    vec4 position7;
+    float threshold;
+} vs_out;
+
+void main(void)
+{
+    vs_out.position0 = position0;
+    vs_out.position1 = position1;
+    vs_out.position2 = position2;
+    vs_out.position3 = position3;
+    vs_out.position4 = position4;
+    vs_out.position5 = position5;
+    vs_out.position6 = position6;
+    vs_out.position7 = position7;
+
+    vs_out.threshold = threshold;
+}
